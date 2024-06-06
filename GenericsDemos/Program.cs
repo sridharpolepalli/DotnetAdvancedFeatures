3631e729d473fab9821418ac19e48f119683ed3e
sridhar_polepalli <sridhar_polepalli@epam.com>
2024-06-06 09:06:23 +0530

    commented

--- GenericsDemos/Program.cs
+++ GenericsDemos/Program.cs
@@ -9,35 +9,35 @@ namespace GenericsDemos
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            // Configure Serilog
-            Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Debug()
-                .WriteTo.Console()
-                .WriteTo.File("logs\\myapp.txt", rollingInterval: RollingInterval.Day)
-                .CreateLogger();
+        //static void Main(string[] args)
+        //{
+        //    // Configure Serilog
+        //    Log.Logger = new LoggerConfiguration()
+        //        .MinimumLevel.Debug()
+        //        .WriteTo.Console()
+        //        .WriteTo.File("logs\\myapp.txt", rollingInterval: RollingInterval.Day)
+        //        .CreateLogger();
 
-            // Example logging
-            Log.Information("Application is starting...");
+        //    // Example logging
+        //    Log.Information("Application is starting...");
 
-            try
-            {
-                // Your application code here
-                Log.Debug("Debugging...");
-                Log.Warning("This is a warning...");
-                GenericClassDemo1.TestMethod();
-            }
-            catch (Exception ex)
-            {
-                Log.Error(ex, "An unexpected error occurred");
-            }
+        //    try
+        //    {
+        //        // Your application code here
+        //        Log.Debug("Debugging...");
+        //        Log.Warning("This is a warning...");
+        //        GenericClassDemo1.TestMethod();
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Log.Error(ex, "An unexpected error occurred");
+        //    }
 
-            Log.Information("Application is closing...");
+        //    Log.Information("Application is closing...");
 
-            // Flush and close log
-            Log.CloseAndFlush();
+        //    // Flush and close log
+        //    Log.CloseAndFlush();
 
-        }
+        //}
     }
 }
